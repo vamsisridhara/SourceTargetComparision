64a305e38425a7b5613374fd1f8e1bea52ab60e5
Vamsi Krishna Sridhara <vamsisridhara@gmail.com>
2017-03-08 21:25:16 -0500

    remove # by adding base href

--- SourceTargetComparision/app/app.routes.js
+++ SourceTargetComparision/app/app.routes.js
@@ -1,8 +1,13 @@
 ﻿(function () {
     'use strict';
     var routerApp = angular.module('sourcetargetroute', []);
-    routerApp.config(function ($stateProvider, $urlRouterProvider) {
+    routerApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $locationProvider.hashPrefix('').html5Mode({
+            enabled: true,
+            requireBase: false
+        });
         $urlRouterProvider.otherwise('/home');
+        //$locationProvider.html5Mode(true);
         $stateProvider.state('home', {
             url: '/home',
             templateUrl: './home.html'
